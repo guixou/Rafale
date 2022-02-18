d7d314890766806777f2caf9aede183fe69d1d08
guixou <guillaumejuju@live.fr>
2022-02-18 12:39:36 +0100

    fin de la V1
    ajout de tout les lien et de modal + responsive

--- public/js/ShowImg.js
+++ public/js/ShowImg.js
@@ -1,3 +1,4 @@
+//tout charger
 async function loadElements() {
 
     const response = await fetch("public/data/data.json")
@@ -6,6 +7,7 @@ async function loadElements() {
     return pictures
 };
 
+// afficher toute les photo
 function displayElements(pictures) {
 
     const section = document.querySelector(".grid") 
@@ -26,6 +28,8 @@ function displayElements(pictures) {
 
     const elements = document.querySelectorAll("#showElement");
     
+    // afficher le pop up/modal 
+
     elements.forEach((element) => {
         element.addEventListener("click", function(){
             const id = element.getAttribute("data-id");
@@ -37,12 +41,60 @@ function displayElements(pictures) {
     
 };
 
+// afficher le pop up/modal 
+
+const modal = document.querySelector(".modal")
+
 
 function show(id, desc, img){
-    console.log(id, desc, img)
-
+    const modalPhoto = document.querySelector(".modalPhoto")
+    const modalText = document.querySelector(".modalText")
+    // console.log(id, desc, img)
+ 
+    modal.classList.add("modalActive")
+    modalPhoto.setAttribute("src", img)
+    modalText.innerHTML= desc
 };
 
+// cacher la modal
+
+modal.addEventListener('click', function(){
+    modal.classList.remove("modalActive")
+});
+
+// afficher le dernier projet
+
+const LastProject = document.querySelector(".LastProject")
+LastProject.addEventListener('click', function(){
+    show(3, "station de lavage et décapotable", "public/image/3.jpg")
+});
+
+// afficher le projet phare
+
+const bestProject = document.querySelector(".bestProject")
+bestProject.addEventListener('click', function(){
+    show(14, "TRIPLE S", "public/image/14.jpg")
+});
+
+const random = document.querySelector(".random")
+random.addEventListener('click', async function(){
+    let id = getRandom(1, 23)
+    let img = `public/image/${id}.jpg`
+
+    
+    const pictures = await loadElements()
+    const fdp = pictures[id-1]
+    const desc = fdp.description
+
+
+    function getRandom(min, max) {
+        return Math.floor(Math.random() * (max - min)) + min;
+    }
+
+    show(id, desc, img)
+});
+
+// attendre le chargement final
 async function init() {
     const pictures = await loadElements()
     displayElements(pictures)
